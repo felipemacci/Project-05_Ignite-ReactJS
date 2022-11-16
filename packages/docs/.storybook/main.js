127f3a3ebe2a41f69aaeda8d0957c1b44b1a576d
Felipe Macci <felipemacci@gmail.com>
2022-11-16 17:40:52 -0300

    Add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/Project-05_Ignite-ReactJS/'
+    }
+
+    return config
   }
 }
